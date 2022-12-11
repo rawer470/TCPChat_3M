bcd0e290054084b5e030ffb79f86e3e348cd55e9
rawer470 <75203120+rawer470@users.noreply.github.com>
2022-12-11 17:41:51 +0300

    Initial commit

---
+++ TcpChat_Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChat_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            Console.WriteLine("[CLIENT]");
+
+            Socket socket_sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endRemoitePoint = new IPEndPoint(address, 7632);
+
+            Console.WriteLine("Нажмите Enter для подключения");
+            Console.ReadLine();
+            socket_sender.Connect(endRemoitePoint);
+            while (true)
+            {
+
+                Console.WriteLine("Введите сообщение для отправки на сервер:");
+                string message = Console.ReadLine();
+                byte[] bytes = Encoding.Unicode.GetBytes(message);
+                socket_sender.Send(bytes);
+                Console.WriteLine("Посылка\"" + message + "\" отправлена на сервер!");
+
+                byte[] byte_answers = new byte[1024];
+                int num_bytes = socket_sender.Receive(byte_answers);
+                string answer = Encoding.Unicode.GetString(byte_answers, 0, num_bytes);
+                Console.WriteLine(answer);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpChat_Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChat_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            Console.WriteLine("[SERVER]");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(address, 7632);
+            socket.Bind(endPoint);
+            socket.Listen(1);
+            Console.WriteLine("Ожидаем звонка от клиента...");
+            Socket socket_client = socket.Accept();
+            Console.WriteLine("Клиент на связи!");
+            byte[] bytes = new byte[1024];
+           
+            while (true)
+            {
+                int num_bytes = socket_client.Receive(bytes);
+                string textFromClient = Encoding.Unicode.GetString(bytes, 0, num_bytes);
+                Console.WriteLine(textFromClient);
+
+
+                //ответное сообщение т сервера клиенту
+                string answer = "Server: OK";
+                
+                byte[] bytes_answer = Encoding.Unicode.GetBytes(answer);
+                socket_client.Send(bytes_answer);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
